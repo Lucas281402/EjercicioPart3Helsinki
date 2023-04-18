6d289fb5870e9fccde386268d1279cc7a404c498
Lucas <lucas_river_2814@hotmail.com>
2023-04-18 11:11:46 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-app.use(express.static('buil'))
+app.use(express.static('build'))
 
 let persons = [
   {
